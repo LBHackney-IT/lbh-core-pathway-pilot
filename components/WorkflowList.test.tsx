f075498f4907c2ca3158caddfb99e5f61cd05dd9
Jaye Hackett <jaye.hackett@gmail.com>
2021-07-31 23:54:11 +0100

    add tests

---
+++ components/WorkflowList.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react"
+import { mockWorkflow } from "../fixtures/workflows"
+import WorkflowList from "./WorkflowList"
+
+const mockWorkflows = [mockWorkflow, mockWorkflow, mockWorkflow]
+
+describe("WorkflowList", () => {
+  it("renders a list of panels", () => {
+    render(<WorkflowList workflows={mockWorkflows} />)
+    expect(screen.getAllByRole("heading").length).toBe(3)
+  })
+
+  it("behaves when there are no results to show", () => {
+    render(<WorkflowList workflows={[]} />)
+    expect(screen.getByText("No results to show"))
+  })
+
+  it("shows the total number of results", () => {
+    render(<WorkflowList workflows={mockWorkflows} />)
+    expect(screen.getByText("Showing 3 results"))
+  })
+})
