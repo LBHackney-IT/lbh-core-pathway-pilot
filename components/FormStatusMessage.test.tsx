21a52e783d744e6382847c1f0acf6a5e06e1d929
Jaye Hackett <jaye.hackett@gmail.com>
2021-08-13 12:01:58 +0100

    add tests

---
+++ components/FormStatusMessage.test.tsx
@@ -0,0 +1,30 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import { Formik } from "formik"
+import FormStatusMessage from "./FormStatusMessage"
+
+describe("FormStatusMessage", () => {
+  it("shows nothing initially", () => {
+    render(
+      <Formik initialValues={{}} onSubmit={jest.fn()}>
+        <FormStatusMessage />
+      </Formik>
+    )
+    expect(screen.queryByRole("alert")).toBeNull()
+  })
+
+  it("renders the text of a status", () => {
+    render(
+      <Formik initialValues={{}} onSubmit={jest.fn()}>
+        {({ setStatus }) => (
+          <>
+            <button onClick={() => setStatus("test error")} />
+            <FormStatusMessage />
+          </>
+        )}
+      </Formik>
+    )
+    fireEvent.click(screen.getByRole("button"))
+    expect(screen.getByRole("alert"))
+    expect(screen.getByText("test error"))
+  })
+})
