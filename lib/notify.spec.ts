c1ea8f5b922f76ec862fa9ef4135412d54dbfcdd
Jaye Hackett <jaye.hackett@gmail.com>
2021-08-15 13:02:52 +0100

    notify wip

---
+++ lib/notify.spec.ts
@@ -0,0 +1,72 @@
+import { notifyReturnedForEdits } from "./notify"
+import { NotifyClient } from "notifications-node-client"
+import { waitFor } from "@testing-library/react"
+import { mockUser } from "../fixtures/users"
+import { mockWorkflowWithExtras } from "../fixtures/workflows"
+
+const mockSend = jest.fn()
+
+jest.mock("notifications-node-client", () => {
+  return {
+    NotifyClient: jest.fn().mockImplementation(() => {
+      return { sendEmail: mockSend }
+    }),
+  }
+})
+
+beforeEach(() => {
+  NotifyClient.mockClear()
+})
+
+// describe("notifyApprover", () => {
+//   it("correctly calls the notify client", async () => {
+//     await notifyApprover(mockSubmission, "bar", "http://example.com")
+//     await waitFor(() => {
+//       expect(mockSend).toBeCalledTimes(1)
+//       expect(mockSend).toBeCalledWith(
+//         process.env.NOTIFY_APPROVER_TEMPLATE_ID,
+//         "bar",
+//         {
+//           personalisation: {
+//             form_name: "Sandbox form",
+//             resident_name: "Foo Bar",
+//             resident_social_care_id: "1",
+//             started_by: "foo.bar@hackney.gov.uk",
+//             url: "http://example.com/people/1/submissions/123",
+//           },
+//           reference: "123-bar",
+//         }
+//       )
+//     })
+//   })
+// })
+
+describe("notifyReturnedForEdits", () => {
+  it("correctly calls the notify client", async () => {
+    await notifyReturnedForEdits(
+      mockWorkflowWithExtras,
+      mockUser,
+      "http://example.com",
+      "my reason"
+    )
+    await waitFor(() => {
+      expect(mockSend).toBeCalledTimes(1)
+      expect(mockSend).toBeCalledWith(
+        process.env.NOTIFY_RETURN_FOR_EDITS_TEMPLATE_ID,
+        "firstname.surname@hackney.gov.uk",
+        {
+          personalisation: {
+            rejecter_email: "Firstname Surname",
+            reason: "my reason",
+            // resident_name: "Foo Bar",
+            form_name: "Care act assessment",
+            started_by: "Firstname Surname",
+            url: "http://example.com/workflows/123abc",
+            resident_social_care_id: "123",
+          },
+          reference: "123abc-firstname.surname@hackney.gov.uk",
+        }
+      )
+    })
+  })
+})
---
+++ lib/notify.ts
@@ -0,0 +1,60 @@
+import { NotifyClient } from "notifications-node-client"
+import forms from "../config/forms"
+import { WorkflowWithExtras } from "../types"
+import { User } from "next-auth"
+
+/** send an email notification to someone that a submission needs their approval */
+// export const notifyApprover = async (
+//   submission: Submission,
+//   approverEmail: string,
+//   host: string
+// ): Promise<void> => {
+//   const notifyClient = new NotifyClient(process.env.NOTIFY_API_KEY)
+
+//   return await notifyClient.sendEmail(
+//     process.env.NOTIFY_APPROVER_TEMPLATE_ID,
+//     approverEmail,
+//     {
+//       personalisation: {
+//         url: `${host}/people/${submission.residents[0].id}/submissions/${submission.submissionId}`,
+//         form_name: forms.find(form => form.id === submission.formId)?.name,
+//         resident_name: submission.residents
+//           .map(res => `${res.firstName} ${res.lastName}`)
+//           .join(", "),
+//         resident_social_care_id: submission.residents
+//           .map(res => res.id)
+//           .join(", "),
+//         started_by: submission.createdBy.email,
+//       },
+//       reference: `${submission.submissionId}-${approverEmail}`,
+//     }
+//   )
+// }
+
+/** send an email notification to an assignee that their submission has been rejected */
+export const notifyReturnedForEdits = async (
+  workflow: WorkflowWithExtras,
+  rejector: User,
+  host: string,
+  rejectionReason?: string
+): Promise<void> => {
+  const notifyClient = new NotifyClient(process.env.NOTIFY_API_KEY)
+  return await notifyClient.sendEmail(
+    process.env.NOTIFY_RETURN_FOR_EDITS_TEMPLATE_ID,
+    workflow.assignedTo,
+    {
+      personalisation: {
+        url: `${host}/workflows/${workflow.id}`,
+        form_name: forms.find(form => form.id === workflow.formId)?.name,
+        // resident_name: submission.residents
+        //   .map(res => `${res.firstName} ${res.lastName}`)
+        //   .join(", "),
+        resident_social_care_id: workflow.socialCareId,
+        started_by: workflow?.creator?.name,
+        rejecter_email: rejector.name,
+        reason: rejectionReason,
+      },
+      reference: `${workflow.id}-${rejector.email}`,
+    }
+  )
+}
