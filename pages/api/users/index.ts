191510b3667ae65390f42f92b7ffcf1ac422cd78
Jaye <joshua.hackett93@gmail.com>
2021-08-17 14:17:18 +0100

    add edit users form

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,10 +1,30 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import { apiHandler } from "../../../lib/apiHelpers"
+import { NextApiResponse } from "next"
+import { apiHandler, ApiRequestWithSession } from "../../../lib/apiHelpers"
 import prisma from "../../../lib/prisma"
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const users = await prisma.user.findMany()
-  res.json(users)
+const handler = async (req: ApiRequestWithSession, res: NextApiResponse) => {
+  switch (req.method) {
+    case "PATCH": {
+      if (!req.session.user.approver)
+        return res
+          .status(400)
+          .json({ error: "You're not authorised to perform that action" })
+
+      const users = await prisma.user.updateMany({ data: {}, where: {} })
+      res.json(users)
+      break
+    }
+
+    case "GET": {
+      const users = await prisma.user.findMany()
+      res.json(users)
+      break
+    }
+
+    default: {
+      res.status(405).json({ error: "Method not supported on this endpoint" })
+    }
+  }
 }
 
 export default apiHandler(handler)
--- pages/users.tsx
+++ pages/users.tsx
@@ -4,12 +4,26 @@ import { getSession } from "next-auth/client"
 import prisma from "../lib/prisma"
 import { UserWithSession } from "../types"
 import { prettyDateToNow } from "../lib/formatters"
+import { Field, Form, Formik } from "formik"
+import FormStatusMessage from "../components/FormStatusMessage"
+import { Team } from "@prisma/client"
+import s from "../styles/Users.module.scss"
 
 const UsersPage = ({
   users,
 }: {
   users: UserWithSession[]
 }): React.ReactElement => {
+  const handleSubmit = () => {}
+
+  const initialValues = users.reduce((acc, user) => {
+    acc[user.id] = {
+      team: user.team,
+      approver: user.approver,
+    }
+    return acc
+  }, {})
+
   return (
     <Layout
       title="Team members"
@@ -21,45 +35,89 @@ const UsersPage = ({
     >
       <h1 className="govuk-!-margin-bottom-8">Users</h1>
 
-      <table className="govuk-table lbh-table">
-        <thead className="govuk-table__head">
-          <tr className="govuk-table__row">
-            <th scope="col" className="govuk-table__header">
-              User
-            </th>
-            <th scope="col" className="govuk-table__header">
-              Team
-            </th>
-            <th scope="col" className="govuk-table__header">
-              Approver?
-            </th>
-            <th scope="col" className="govuk-table__header">
-              Last seen
-            </th>
-          </tr>
-        </thead>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        {({ isSubmitting }) => (
+          <Form>
+            <FormStatusMessage />
 
-        <tbody className="govuk-table__body">
-          {users.map(user => (
-            <tr key={user.id} className="govuk-table__row">
-              <th scope="row" className="govuk-table__cell">
-                <p>
-                  <strong>{user.name}</strong>
-                </p>
+            <table className={`govuk-table lbh-table ${s.table}`}>
+              <thead className="govuk-table__head">
+                <tr className="govuk-table__row">
+                  <th scope="col" className="govuk-table__header">
+                    User
+                  </th>
+                  <th scope="col" className="govuk-table__header">
+                    Team
+                  </th>
+                  <th scope="col" className="govuk-table__header">
+                    Approver?
+                  </th>
+                  <th scope="col" className="govuk-table__header">
+                    Last seen
+                  </th>
+                </tr>
+              </thead>
 
-                <p className="lbh-body-xs govuk-!-margin-top-0">{user.email}</p>
-              </th>
-              <td className="govuk-table__cell">{user.team || "No team"}</td>
-              <td className="govuk-table__cell">
-                {user.approver ? "Yes" : "No"}
-              </td>
-              <td className="govuk-table__cell">
-                {prettyDateToNow(String(user.sessions?.[0]?.createdAt))}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+              <tbody className="govuk-table__body">
+                {users.map(user => (
+                  <tr key={user.id} className="govuk-table__row">
+                    <th scope="row" className="govuk-table__cell">
+                      <p>
+                        <strong>{user.name}</strong>
+                      </p>
+
+                      <p className="lbh-body-xs govuk-!-margin-top-0">
+                        {user.email}
+                      </p>
+                    </th>
+
+                    <td className="govuk-table__cell">
+                      <Field
+                        as="select"
+                        name={`${user.id}.team`}
+                        className="govuk-select lbh-select"
+                      >
+                        <option value="">No team</option>
+                        {Object.entries(Team).map(([key, val]) => (
+                          <option key={val} value={val}>
+                            {key}
+                          </option>
+                        ))}
+                      </Field>
+                    </td>
+
+                    <td className="govuk-table__cell">
+                      <div className="govuk-checkboxes__item">
+                        <Field
+                          type="checkbox"
+                          name={`${user.id}.approver`}
+                          className="govuk-checkboxes__input"
+                        />
+                        <label
+                          className="govuk-label govuk-checkboxes__label"
+                          htmlFor="nationality"
+                        >
+                          <span className="govuk-visually-hidden">
+                            Approver?
+                          </span>
+                        </label>
+                      </div>
+                    </td>
+
+                    <td className="govuk-table__cell">
+                      {prettyDateToNow(String(user.sessions?.[0]?.createdAt))}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+
+            <button disabled={isSubmitting} className="govuk-button lbh-button">
+              Save changes
+            </button>
+          </Form>
+        )}
+      </Formik>
     </Layout>
   )
 }
