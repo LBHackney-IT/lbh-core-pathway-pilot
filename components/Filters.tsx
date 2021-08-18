6ee346a51d7e0ef8fcd6034f3e3a52b3f796bf5d
Jaye Hackett <jaye.hackett@gmail.com>
2021-08-18 18:00:54 +0100

    fix bug

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -17,8 +17,8 @@ const Filters = (): React.ReactElement => {
   const [session] = useSession()
   const approver = session?.user?.approver
 
-  const [status, setStatus] = useQueryState<string>("status", "All")
-  const [formId, setFormId] = useQueryState<string>("form_id", "All")
+  const [status, setStatus] = useQueryState<string>("status", "ALL")
+  const [formId, setFormId] = useQueryState<string>("form_id", "ALL")
   const [onlyReviews, setOnlyReviews] = useQueryState<boolean>(
     "only_reviews_reassessments",
     false
