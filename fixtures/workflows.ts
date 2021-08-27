2dcdc08dd0e76fba7456ba0955011d680b9689ae
Jaye <joshua.hackett93@gmail.com>
2021-08-27 13:54:36 +0100

    fix types

--- fixtures/workflows.ts
+++ fixtures/workflows.ts
@@ -31,7 +31,6 @@ export const mockWorkflow: Workflow = {
   discardedAt: null,
   discardedBy: null,
   heldAt: null,
-  nextSteps: [],
 }
 
 export interface MockWorkflowWithExtras extends Workflow {
