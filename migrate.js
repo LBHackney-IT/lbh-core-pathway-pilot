972724960cac5396dcca5cc46365334bfc8ad04e
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-09-21 13:26:53 +0100

    Fix linting errors

--- migrate.js
+++ migrate.js
@@ -1,11 +1,3 @@
-const {promisify} = require('util');
-const exec = promisify(require('child_process').exec);
-
 module.exports.handler = async () => {
     throw new Error();
-    // const output = await exec('node_modules/.bin/prisma db push --skip-generate');
-    //
-    // if (output.stderr.length > 0) console.error(output.stderr);
-    //
-    // return output.stdout;
 };
