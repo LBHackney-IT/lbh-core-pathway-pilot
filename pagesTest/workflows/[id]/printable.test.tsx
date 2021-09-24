4191db44ad0c7df1b21bfbf25e30f8c9eb640f19
Wen Ting Wang <wenting@madetech.com>
2021-09-24 09:42:46 +0100

    Add test for printable workflow page

---
+++ pagesTest/workflows/[id]/printable.test.tsx
@@ -0,0 +1,34 @@
+import { GetServerSidePropsContext } from "next"
+import { mockForm } from "../../../fixtures/form"
+import { mockResident } from "../../../fixtures/residents"
+import { mockWorkflowWithExtras } from "../../../fixtures/workflows"
+import { ParsedUrlQuery } from "querystring"
+import { getResidentById } from "../../../lib/residents"
+import { getServerSideProps } from "../../../pages/workflows/[id]/printable"
+
+jest.mock("../../../lib/prisma", () => ({
+  workflow: {
+    findUnique: jest.fn().mockResolvedValue(mockWorkflowWithExtras),
+  },
+}))
+
+jest.mock("../../../lib/residents")
+;(getResidentById as jest.Mock).mockResolvedValue(mockResident)
+
+describe("getServerSideProps", () => {
+  it("returns the workflow and form as props", async () => {
+    const response = await getServerSideProps({
+      query: {
+        social_care_id: mockResident.mosaicId,
+      } as ParsedUrlQuery,
+    } as GetServerSidePropsContext)
+
+    expect(response).toHaveProperty(
+      "props",
+      expect.objectContaining({
+        id: mockWorkflowWithExtras.id,
+        form: mockForm,
+      })
+    )
+  })
+})
