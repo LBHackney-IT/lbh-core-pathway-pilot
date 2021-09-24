57dbc766045fae992627205403ca0f330ad3f9f1
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-09-24 11:16:14 +0100

    Don't log migration script output to github actions

--- migrate.js
+++ migrate.js
@@ -4,5 +4,7 @@ const exec = promisify(require('child_process').exec);
 module.exports.handler = async () => {
     const output = await exec('node_modules/.bin/prisma db push --skip-generate');
 
-    return output.stdout;
+    console.log(output.stdout);
+
+    return true;
 };
