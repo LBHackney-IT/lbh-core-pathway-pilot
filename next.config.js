75406766a925a9edebb5741d8598c67eb7e1f8b4
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-09-27 14:28:43 +0100

    Allow google tag manager

--- next.config.js
+++ next.config.js
@@ -12,10 +12,10 @@ module.exports = {
       patterns: [
         {
           from: join(__dirname, 'public/favicon.ico'),
-          to: join(__dirname, 'build/')
-        }
-      ]
-    }))
+          to: join(__dirname, 'build/'),
+        },
+      ],
+    }));
 
     return config
   },
@@ -33,11 +33,13 @@ module.exports = {
             value: "nosniff",
           },
           {
-            key: "Content-Security-Policy",
+            key: 'Content-Security-Policy',
             value:
               "default-src 'self' hackney.gov.uk *.hackney.gov.uk;" +
               "object-src 'none';" +
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline';" +
+              "connect-src 'self' www.google-analytics.com;" +
+              "script-src-elem 'self' 'unsafe-inline' www.googletagmanager.com;" +
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' www.googletagmanager.com;" +
               "style-src 'self' 'unsafe-inline' fonts.googleapis.com fonts.gstatic.com;" +
               "font-src 'self' fonts.googleapis.com fonts.gstatic.com;" +
               "frame-ancestors 'self';" +
