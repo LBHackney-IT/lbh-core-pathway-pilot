ba0c7614ca45c58beb86007516ff10aa900fe28d
Wen Ting Wang <wenting@madetech.com>
2021-09-30 14:35:43 +0100

    Refactor primary actions tests to stub getStatus
    This way we don't have to update the workflow used in the tests
    to match the exact conditions for the status we want to use for
    the workflow.

--- components/PrimaryAction.test.tsx
+++ components/PrimaryAction.test.tsx
@@ -2,8 +2,16 @@ import { render, screen } from "@testing-library/react"
 import { useSession } from "next-auth/client"
 import { useRouter } from "next/router"
 import { mockApprover, mockPanelApprover, mockUser } from "../fixtures/users"
-import { mockWorkflow, MockWorkflowWithExtras } from "../fixtures/workflows"
+import {
+  mockWorkflow,
+  MockWorkflowWithExtras,
+} from "../fixtures/workflows"
 import PrimaryAction from "./PrimaryAction"
+import { getStatus } from "../lib/status"
+import { Status } from "../types"
+
+jest.mock("../lib/status")
+;(getStatus as jest.Mock).mockReturnValue(Status.InProgress)
 
 jest.mock("next/router")
 ;(useRouter as jest.Mock).mockReturnValue({
@@ -14,102 +22,72 @@ jest.mock("next-auth/client")
 ;(useSession as jest.Mock).mockReturnValue([{ user: mockUser }, false])
 
 describe("PrimaryAction", () => {
+  beforeEach(() => {
+    ;(getStatus as jest.Mock).mockClear()
+  })
+
   it("shows a resume button for an in-progress workflow", () => {
+    ;(getStatus as jest.Mock).mockReturnValue(Status.InProgress)
+
     render(<PrimaryAction workflow={mockWorkflow as MockWorkflowWithExtras} />)
+
     expect(screen.getByText("Resume"))
   })
 
   it("shows a review button for a finished workflow", () => {
-    render(
-      <PrimaryAction
-        workflow={
-          {
-            ...mockWorkflow,
-            panelApprovedAt: new Date(),
-          } as MockWorkflowWithExtras
-        }
-      />
-    )
+    ;(getStatus as jest.Mock).mockReturnValue(Status.NoAction)
+
+    render(<PrimaryAction workflow={mockWorkflow as MockWorkflowWithExtras} />)
     expect(screen.getByText("Start reassessment"))
   })
 
   it("doesn't show the approve button if the user is not an approver", () => {
-    render(
-      <PrimaryAction
-        workflow={
-          {
-            ...mockWorkflow,
-            submittedAt: new Date(),
-          } as MockWorkflowWithExtras
-        }
-      />
-    )
+    ;(getStatus as jest.Mock).mockReturnValue(Status.Submitted)
+
+    render(<PrimaryAction workflow={mockWorkflow as MockWorkflowWithExtras} />)
+
     expect(screen.queryByText("Make a decision")).toBeNull()
     expect(screen.queryByRole("button")).toBeNull()
   })
 
   it("shows the approve button if the user is an approver", () => {
+    ;(getStatus as jest.Mock).mockReturnValue(Status.Submitted)
     ;(useSession as jest.Mock).mockReturnValue([{ user: mockApprover }, false])
-    render(
-      <PrimaryAction
-        workflow={
-          {
-            ...mockWorkflow,
-            submittedAt: new Date(),
-          } as MockWorkflowWithExtras
-        }
-      />
-    )
+
+    render(<PrimaryAction workflow={mockWorkflow as MockWorkflowWithExtras} />)
+
     expect(screen.getByText("Make a decision"))
     expect(screen.getByRole("button"))
   })
 
   it("doesn't show the panel approve button if the user is not an approver", () => {
-    render(
-      <PrimaryAction
-        workflow={
-          {
-            ...mockWorkflow,
-            managerApprovedAt: new Date(),
-          } as MockWorkflowWithExtras
-        }
-      />
-    )
+    ;(getStatus as jest.Mock).mockReturnValue(Status.ManagerApproved)
+
+    render(<PrimaryAction workflow={mockWorkflow as MockWorkflowWithExtras} />)
+
     expect(screen.queryByText("Make a decision")).toBeNull()
     expect(screen.queryByRole("button")).toBeNull()
   })
 
   it("shows the panel approve button if the user is an approver", () => {
+    ;(getStatus as jest.Mock).mockReturnValue(Status.ManagerApproved)
     ;(useSession as jest.Mock).mockReturnValue([
       { user: mockPanelApprover },
       false,
     ])
-    render(
-      <PrimaryAction
-        workflow={
-          {
-            ...mockWorkflow,
-            managerApprovedAt: new Date(),
-          } as MockWorkflowWithExtras
-        }
-      />
-    )
+
+    render(<PrimaryAction workflow={mockWorkflow as MockWorkflowWithExtras} />)
+
     expect(screen.getByText("Make a decision"))
     expect(screen.getByRole("button"))
   })
 
   it("shows a restore button for a discarded workflow", () => {
+    ;(getStatus as jest.Mock).mockReturnValue(Status.Discarded)
     ;(useSession as jest.Mock).mockReturnValue([{ user: mockApprover }, false])
-    render(
-      <PrimaryAction
-        workflow={
-          {
-            ...mockWorkflow,
-            discardedAt: new Date(),
-          } as MockWorkflowWithExtras
-        }
-      />
-    )
+
+    render(<PrimaryAction workflow={mockWorkflow as MockWorkflowWithExtras} />)
+
     expect(screen.getByText("Re-open"))
   })
 })
