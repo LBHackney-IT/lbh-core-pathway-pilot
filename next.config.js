41dbd7756b6b976a8643453fc4e38b1759aff0c5
Jaye Hackett <jaye.hackett@gmail.com>
2021-10-01 15:05:32 +0100

    nice redirect

--- next.config.js
+++ next.config.js
@@ -1,21 +1,30 @@
-const {join} = require('path')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { join } = require("path")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
-  distDir: 'build/_next',
-  target: 'server',
+  distDir: "build/_next",
+  target: "server",
   poweredByHeader: false,
-  webpack: (config, {isServer}) => {
-    if (isServer) return config;
+  redirects: () => [
+    {
+      source: "/workflows",
+      destination: "/",
+      permanent: true,
+    },
+  ],
+  webpack: (config, { isServer }) => {
+    if (isServer) return config
 
-    config.plugins.push(new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: join(__dirname, 'public/favicon.ico'),
-          to: join(__dirname, 'build/'),
-        },
-      ],
-    }));
+    config.plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: join(__dirname, "public/favicon.ico"),
+            to: join(__dirname, "build/"),
+          },
+        ],
+      })
+    )
 
     return config
   },
@@ -37,20 +46,21 @@ module.exports = {
             value: "max-age=63072000; includeSubDomains; preload",
           },
           {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
           },
         ],
       },
-    ];
+    ]
 
-    if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === "production") {
       headers[0].headers.push({
-        key: 'Content-Security-Policy',
-        value: "default-src 'self'; frame-ancestors 'self'; form-action 'self';",
-      });
+        key: "Content-Security-Policy",
+        value:
+          "default-src 'self'; frame-ancestors 'self'; form-action 'self';",
+      })
     }
 
-    return headers;
+    return headers
   },
 }
