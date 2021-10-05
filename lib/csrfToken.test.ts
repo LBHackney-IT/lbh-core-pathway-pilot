2de756f98377e3af0eedef3d228bde12c76a8a96
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-10-05 13:33:48 +0100

    Add CSRF token generation and protect users api endpoint

---
+++ lib/csrfToken.test.ts
@@ -0,0 +1,97 @@
+import {CSRFValidationError, init} from "./csrfToken";
+import {ApiRequestWithSession} from "./apiHelpers";
+import {NextApiRequest, NextApiResponse} from "next";
+import {mockUser} from "../fixtures/users";
+
+export const handler =
+  async (req: ApiRequestWithSession, res: NextApiResponse): Promise<void> => {
+    res.status(200).json({});
+  }
+
+process.env.CSRF_SECRET = 'test-secret';
+
+const csrf = init();
+const csrf2 = init();
+
+describe('validate', () => {
+  test('when the token is valid', () => {
+    expect(() => csrf.validate(csrf.token())).not.toThrow();
+  });
+  test('when the token is invalid', () => {
+    expect(() => csrf.validate('not-a-valid-token')).toThrow(CSRFValidationError);
+  });
+  test('when the token is generate by another instance', () => {
+    expect(() => csrf.validate(csrf2.token())).not.toThrow();
+  });
+});
+
+describe('middleware', () => {
+  const csrfHandler = csrf.middleware(handler);
+  let response: NextApiResponse;
+
+  beforeEach(() => {
+    response = {
+      status: jest.fn().mockImplementation(() => response),
+      json: jest.fn(),
+    } as unknown as NextApiResponse
+  })
+
+  describe('a valid request', () => {
+    const request = {
+      method: "POST",
+      body: JSON.stringify({}),
+      session: {user: mockUser},
+      headers: {
+        'xsrf-token': csrf.token(),
+      },
+    } as unknown as ApiRequestWithSession
+
+    test('allows the request through', async () => {
+      await csrfHandler(request, response);
+
+      expect(response.status).toHaveBeenCalledWith(200);
+    });
+  });
+
+  describe('an invalid request', () => {
+    const error = console.error;
+
+    beforeAll(() => {console.error = jest.fn()});
+    afterAll(() => {console.error = error});
+
+    const request = {
+      method: "POST",
+      body: JSON.stringify({}),
+      session: {user: mockUser},
+      url: "/test-url",
+    } as unknown as ApiRequestWithSession
+
+    test('stops a request with no header', async () => {
+      await csrfHandler(request, response);
+
+      expect(response.status).toHaveBeenCalledWith(403);
+      expect(response.json).toHaveBeenCalledWith({
+        error: "invalid csrf token"
+      });
+      expect(console.error).toHaveBeenCalledWith(
+        "[xsrf][error] invalid token on request to /test-url: Cannot read property 'xsrf-token' of undefined"
+      )
+    });
+
+    test('stops a request with an invalid token', async () => {
+      await csrfHandler({
+        ...request,
+        headers: {'XSRF-TOKEN': 'invalid-token'},
+        url: "/test-url",
+      } as unknown as NextApiRequest, response);
+
+      expect(response.status).toHaveBeenCalledWith(403);
+      expect(response.json).toHaveBeenCalledWith({
+        error: "invalid csrf token"
+      });
+      expect(console.error).toHaveBeenCalledWith(
+        "[xsrf][error] invalid token on request to /test-url: invalid csrf token provided"
+      )
+    });
+  });
+})
