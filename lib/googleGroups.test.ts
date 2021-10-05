e87e23b2032f171a79c1db811252a313ecdf750f
Wen Ting Wang <wenting@madetech.com>
2021-10-05 10:32:01 +0100

    Update tests for checkAuthorisedToLogin to consider environment

--- lib/googleGroups.test.ts
+++ lib/googleGroups.test.ts
@@ -7,42 +7,87 @@ import { NextApiRequest } from "next"
 process.env.GSSO_TOKEN_NAME = "foo"
 process.env.HACKNEY_JWT_SECRET = "secret"
 
-describe("checkGoogleToken", () => {
-  it("correctly handles an authorised user", async () => {
-    const mockRes = {
-      headers: {
-        cookie: cookie.serialize(
-          process.env.GSSO_TOKEN_NAME,
-          jwt.sign(
-            {
-              groups: [allowedGroups[0]],
-            },
-            process.env.HACKNEY_JWT_SECRET
-          )
-        ),
-      },
-    }
+const switchEnv = environment => {
+  const oldEnv = process.env.NODE_ENV
+  process.env.NEXT_PUBLIC_ENV = environment
+  // @ts-ignore
+  process.env.NODE_ENV = environment
 
-    const result = await checkAuthorisedToLogin(mockRes as NextApiRequest)
-    expect(result).toBeTruthy()
-  })
+  return () => switchEnv(oldEnv)
+}
 
-  it("correctly handles an unauthorised user", async () => {
-    const mockRes = {
-      headers: {
-        cookie: cookie.serialize(
-          process.env.GSSO_TOKEN_NAME,
-          jwt.sign(
-            {
-              groups: [],
-            },
-            process.env.HACKNEY_JWT_SECRET
-          )
-        ),
-      },
-    }
+describe("checkAuthorisedToLogin", () => {
+  describe("when in development", () => {
+    let switchBack;
 
-    const result = await checkAuthorisedToLogin(mockRes as NextApiRequest)
-    expect(result).toBeFalsy()
-  })
+    beforeAll(() => switchBack = switchEnv("development"))
+    afterAll(() => switchBack())
+
+    it("returns true", async () => {
+      const mockRes = {
+        headers: {
+          cookie: cookie.serialize(
+            process.env.GSSO_TOKEN_NAME,
+            jwt.sign(
+              {
+                groups: [allowedGroups[0]],
+              },
+              process.env.HACKNEY_JWT_SECRET
+            )
+          ),
+        },
+      }
+
+      const result = await checkAuthorisedToLogin(mockRes as NextApiRequest)
+
+      expect(result).toBeTruthy()
+    })
+  });
+
+  describe("when in production", () => {
+    let switchBack;
+
+    beforeAll(() => switchBack = switchEnv("production"))
+    afterAll(() => switchBack())
+
+    it("returns true if an authorised user", async () => {
+      const mockRes = {
+        headers: {
+          cookie: cookie.serialize(
+            process.env.GSSO_TOKEN_NAME,
+            jwt.sign(
+              {
+                groups: [allowedGroups[0]],
+              },
+              process.env.HACKNEY_JWT_SECRET
+            )
+          ),
+        },
+      }
+
+      const result = await checkAuthorisedToLogin(mockRes as NextApiRequest)
+
+      expect(result).toBeTruthy()
+    })
+
+    it("returns false if an unauthorised user", async () => {
+      const mockRes = {
+        headers: {
+          cookie: cookie.serialize(
+            process.env.GSSO_TOKEN_NAME,
+            jwt.sign(
+              {
+                groups: [],
+              },
+              process.env.HACKNEY_JWT_SECRET
+            )
+          ),
+        },
+      }
+
+      const result = await checkAuthorisedToLogin(mockRes as NextApiRequest)
+
+      expect(result).toBeFalsy()
+    })
+  });
 })
