1cddbef7ad1bccf6708ff7337790570a1c50ba8f
Wen Ting Wang <wenting@madetech.com>
2021-10-06 12:50:47 +0100

    Refactor isInPilotGroup to take in cookie instead of whole request

--- lib/googleGroups.test.ts
+++ lib/googleGroups.test.ts
@@ -166,21 +166,17 @@ describe("isInPilotGroup", () => {
       afterAll(() => switchBack())
 
       it("returns true", async () => {
-        const mockRes = {
-          headers: {
-            cookie: cookie.serialize(
-              process.env.GSSO_TOKEN_NAME,
-              jwt.sign(
-                {
-                  groups: [allowedGroups[0]],
-                },
-                process.env.HACKNEY_JWT_SECRET
-              )
-            ),
-          },
-        }
+        const mockCookie = cookie.serialize(
+          process.env.GSSO_TOKEN_NAME,
+          jwt.sign(
+            {
+              groups: [allowedGroups[0]],
+            },
+            process.env.HACKNEY_JWT_SECRET
+          )
+        )
 
-        const result = await isInPilotGroup(mockRes as NextApiRequest)
+        const result = await isInPilotGroup(mockCookie)
 
         expect(result).toBeTruthy()
       })
@@ -205,36 +201,28 @@ describe("isInPilotGroup", () => {
     })
 
     it("returns true if user is in pilot group", async () => {
-      const mockRes = {
-        headers: {
-          cookie: cookie.serialize(
-            process.env.GSSO_TOKEN_NAME,
-            jwt.sign(
-              {
-                groups: [pilotGroup],
-              },
-              process.env.HACKNEY_JWT_SECRET
-            )
-          ),
-        },
-      }
+      const mockCookie = cookie.serialize(
+        process.env.GSSO_TOKEN_NAME,
+        jwt.sign(
+          {
+            groups: [pilotGroup],
+          },
+          process.env.HACKNEY_JWT_SECRET
+        )
+      )
 
-      const result = await isInPilotGroup(mockRes as NextApiRequest)
+      const result = await isInPilotGroup(mockCookie)
 
       expect(result).toBeTruthy()
     })
 
     it("returns false if cookie is signed with invalid JWT secret", async () => {
-      const mockRes = {
-        headers: {
-          cookie: cookie.serialize(
-            process.env.GSSO_TOKEN_NAME,
-            jwt.sign({}, "some-invalid-secret")
-          ),
-        },
-      }
+      const mockCookie = cookie.serialize(
+        process.env.GSSO_TOKEN_NAME,
+        jwt.sign({}, "some-invalid-secret")
+      )
 
-      const result = await isInPilotGroup(mockRes as NextApiRequest)
+      const result = await isInPilotGroup(mockCookie)
 
       expect(result).toBe(false)
       expect(console.error).toHaveBeenCalledWith(
@@ -243,16 +231,12 @@ describe("isInPilotGroup", () => {
     })
 
     it("returns false if cookie is signed with invalid cookie name", async () => {
-      const mockRes = {
-        headers: {
-          cookie: cookie.serialize(
-            "some-invalid-cookie-name",
-            jwt.sign({}, process.env.HACKNEY_JWT_SECRET)
-          ),
-        },
-      }
+      const mockCookie = cookie.serialize(
+        "some-invalid-cookie-name",
+        jwt.sign({}, process.env.HACKNEY_JWT_SECRET)
+      )
 
-      const result = await isInPilotGroup(mockRes as NextApiRequest)
+      const result = await isInPilotGroup(mockCookie)
 
       expect(result).toBe(false)
       expect(console.error).toHaveBeenCalledWith(
@@ -261,36 +245,27 @@ describe("isInPilotGroup", () => {
     })
 
     it("returns false if user has no groups", async () => {
-      const mockRes = {
-        headers: {
-          cookie: cookie.serialize(
-            process.env.GSSO_TOKEN_NAME,
-            jwt.sign({ groups: [] }, process.env.HACKNEY_JWT_SECRET)
-          ),
-        },
-      }
+      const mockCookie = cookie.serialize(
+        process.env.GSSO_TOKEN_NAME,
+        jwt.sign({ groups: [] }, process.env.HACKNEY_JWT_SECRET)
+      )
 
-      const result = await isInPilotGroup(mockRes as NextApiRequest)
+      const result = await isInPilotGroup(mockCookie)
 
       expect(result).toBe(false)
     })
 
     it("returns false if user has groups but not the pilot group", async () => {
-      const mockRes = {
-        headers: {
-          cookie: cookie.serialize(
-            process.env.GSSO_TOKEN_NAME,
-            jwt.sign(
-              {
-                groups: ["some-non-pilot-group", "another-non-pilot-group"],
-              },
-              process.env.HACKNEY_JWT_SECRET
-            )
-          ),
-        },
-      }
-
-      const result = await isInPilotGroup(mockRes as NextApiRequest)
+      const mockCookie = cookie.serialize(
+        process.env.GSSO_TOKEN_NAME,
+        jwt.sign(
+          {
+            groups: ["some-non-pilot-group", "another-non-pilot-group"],
+          },
+          process.env.HACKNEY_JWT_SECRET
+        )
+      )
+      const result = await isInPilotGroup(mockCookie)
 
       expect(result).toBe(false)
     })
