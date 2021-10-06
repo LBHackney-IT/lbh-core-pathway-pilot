6f14624fef1aa40a459d8f6ee8ed75a745e86ab0
Jaye Hackett <jaye.hackett@gmail.com>
2021-10-06 17:24:10 +0100

    script wip

--- jobs/import-historic-data.js
+++ jobs/import-historic-data.js
@@ -4,11 +4,15 @@ require("dotenv").config()
 
 const run = async () => {
   // 1. fetch mapping sheet
+  console.log("Fetching mapping data...")
   const res = await fetch(process.env.HISTORIC_MAPPING_DATA_SOURCE)
   const text = await res.text()
-  const rows = await csv().fromString(text)
+  const allRows = await csv().fromString(text)
+  const rows = allRows.slice(1)
 
   //   2. find the source forms
+  console.log("Finding source forms...")
+  console.log(rows[0])
 }
 
 run()
