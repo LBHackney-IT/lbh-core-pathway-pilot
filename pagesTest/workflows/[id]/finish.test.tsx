309cc3ff7a8fc214a612b20276ba8a17a742f88a
Wen Ting Wang <wenting@madetech.com>
2021-10-07 15:16:27 +0100

    Add redirect in finish page if user isn't in pilot group

--- pagesTest/workflows/[id]/finish.test.tsx
+++ pagesTest/workflows/[id]/finish.test.tsx
@@ -5,11 +5,16 @@ import { mockWorkflowWithExtras } from "../../../fixtures/workflows"
 import { ParsedUrlQuery } from "querystring"
 import { getResidentById } from "../../../lib/residents"
 import { getServerSideProps } from "../../../pages/workflows/[id]/finish"
+import cookie from "cookie"
+import jwt from "jsonwebtoken"
+import { pilotGroup } from "../../../config/allowedGroups"
+
+process.env.GSSO_TOKEN_NAME = "foo"
+process.env.HACKNEY_JWT_SECRET = "secret"
 
 jest.mock("../../../lib/prisma", () => ({
   workflow: {
     findUnique: jest.fn().mockResolvedValue(mockWorkflowWithExtras),
-    update: jest.fn(),
   },
 }))
 
@@ -20,8 +25,22 @@ describe("getServerSideProps", () => {
   it("returns the workflow and form as props", async () => {
     const response = await getServerSideProps({
       query: {
-        social_care_id: mockResident.mosaicId,
+        id: mockWorkflowWithExtras.id,
       } as ParsedUrlQuery,
+      req: {
+        headers: {
+          referer: "http://example.com",
+          cookie: cookie.serialize(
+            process.env.GSSO_TOKEN_NAME,
+            jwt.sign(
+              {
+                groups: [pilotGroup],
+              },
+              process.env.HACKNEY_JWT_SECRET
+            )
+          ),
+        },
+      },
     } as GetServerSidePropsContext)
 
     expect(response).toHaveProperty(
@@ -31,5 +50,56 @@ describe("getServerSideProps", () => {
         form: mockForm,
       })
     )
+  })
+
+  it("redirects back if user is not in pilot group", async () => {
+    const response = await getServerSideProps({
+      query: {
+        id: mockWorkflowWithExtras.id,
+      } as ParsedUrlQuery,
+      req: {
+        headers: {
+          referer: "http://example.com",
+          cookie: cookie.serialize(
+            process.env.GSSO_TOKEN_NAME,
+            jwt.sign(
+              {
+                groups: ["some-non-pilot-group"],
+              },
+              process.env.HACKNEY_JWT_SECRET
+            )
+          ),
+        },
+      },
+    } as GetServerSidePropsContext)
+
+    expect(response).toHaveProperty("redirect", {
+      destination: "http://example.com",
+    })
+  })
+
+  it("redirects back to if user is not in pilot group and no referer", async () => {
+    const response = await getServerSideProps({
+      query: {
+        id: mockWorkflowWithExtras.id,
+      } as ParsedUrlQuery,
+      req: {
+        headers: {
+          cookie: cookie.serialize(
+            process.env.GSSO_TOKEN_NAME,
+            jwt.sign(
+              {
+                groups: ["some-non-pilot-group"],
+              },
+              process.env.HACKNEY_JWT_SECRET
+            )
+          ),
+        },
+      },
+    } as GetServerSidePropsContext)
+
+    expect(response).toHaveProperty("redirect", {
+      destination: "/",
+    })
   })
 })
