902a16b776c4e6eafe6760b9f3242f50fe983c1b
Wen Ting Wang <wenting@madetech.com>
2021-10-11 17:41:55 +0100

    Update TimetableAnswer to handle new structure
    This will now display the summary stats like total hours, weekly cost
    and annual cost.

--- components/FlexibleAnswers/TimetableAnswer.spec.tsx
+++ components/FlexibleAnswers/TimetableAnswer.spec.tsx
@@ -1,7 +1,8 @@
-import { isTimetableAnswer } from "./TimetableAnswer"
+import TimetableAnswer, { isTimetableAnswer } from "./TimetableAnswer"
+import { render, screen, within } from "@testing-library/react"
 
 describe("isTimetableAnswer", () => {
-  it("returns true if properties at root-level", () => {
+  it("returns true if values are at root-level", () => {
     const result = isTimetableAnswer({
       Mon: {},
       Tue: {},
@@ -16,7 +17,7 @@ describe("isTimetableAnswer", () => {
     expect(result).toBeTruthy()
   })
 
-  it("returns true if timetable and summary properties", () => {
+  it("returns true if values are in a timetable property", () => {
     const result = isTimetableAnswer({
       timetable: {
         Mon: {},
@@ -47,5 +48,86 @@ describe("isTimetableAnswer", () => {
     })
 
     expect(result).toBe(false)
+  })
+})
+
+describe("TimetableAnswer", () => {
+  it("displays the timetable if values are at root-level", () => {
+    const answers = {
+      Mon: { Morning: "15", Afternoon: "30" },
+      Tue: {},
+      Wed: {},
+      Thu: {},
+      Fri: { Evening: "60", Night: "75" },
+      Sat: {},
+      Sun: {},
+      "Any day": {},
+    }
+
+    render(<TimetableAnswer answers={answers} />)
+
+    const mondayRow = screen.getByText("Mon").closest("tr")
+
+    expect(within(mondayRow).getByText(15)).toBeVisible()
+    expect(within(mondayRow).getByText(30)).toBeVisible()
+
+    const fridayRow = screen.getByText("Fri").closest("tr")
+
+    expect(within(fridayRow).getByText(60)).toBeVisible()
+    expect(within(fridayRow).getByText(75)).toBeVisible()
+  })
+
+  it("displays the timetable if values are in a timetable property", () => {
+    const answers = {
+      timetable: {
+        Mon: { Morning: "15", Afternoon: "30" },
+        Tue: {},
+        Wed: {},
+        Thu: {},
+        Fri: { Evening: "60", Night: "75" },
+        Sat: {},
+        Sun: {},
+        "Any day": {},
+      },
+    }
+
+    render(<TimetableAnswer answers={answers} />)
+
+    const mondayRow = screen.getByText("Mon").closest("tr")
+
+    expect(within(mondayRow).getByText(15)).toBeVisible()
+    expect(within(mondayRow).getByText(30)).toBeVisible()
+
+    const fridayRow = screen.getByText("Fri").closest("tr")
+
+    expect(within(fridayRow).getByText(60)).toBeVisible()
+    expect(within(fridayRow).getByText(75)).toBeVisible()
+  })
+
+  it("displays the summary if values are in a timetable property", () => {
+    const answers = {
+      timetable: {
+        Mon: { Morning: "15", Afternoon: "30" },
+        Tue: {},
+        Wed: {},
+        Thu: {},
+        Fri: { Evening: "60", Night: "75" },
+        Sat: {},
+        Sun: {},
+        "Any day": {},
+      },
+      summary: {
+        "annual cost": "£2808",
+        "total hours": "3",
+        "weekly cost": "£54",
+      },
+    }
+
+    render(<TimetableAnswer answers={answers} />)
+
+    expect(screen.getByText("3 hours total")).toBeVisible()
+    expect(
+      screen.getByText("£2808 estimated annual cost (or £54 weekly, at £18/hour)")
+    ).toBeVisible()
   })
 })
