347ef3956569b27762202b4819dbd6f5344c207c
Jaye <joshua.hackett93@gmail.com>
2021-10-11 10:48:13 +0100

    add a little more scaffolding to the historic data import script

--- jobs/import-historic-data.js
+++ jobs/import-historic-data.js
@@ -1,8 +1,12 @@
 const fetch = require("node-fetch")
 const csv = require("csvtojson")
+const { PrismaClient } = require("@prisma/client")
 require("dotenv").config()
 
 const run = async () => {
+  // 0. set up a database connection
+  let db = new PrismaClient()
+
   // 1. fetch mapping sheet
   console.log("Fetching mapping data...")
   const res = await fetch(process.env.HISTORIC_MAPPING_DATA_SOURCE)
@@ -10,9 +14,22 @@ const run = async () => {
   const allRows = await csv().fromString(text)
   const rows = allRows.slice(1)
 
-  //   2. find the source forms
+  // 2. get a list of all the unique response spreadsheets
   console.log("Finding source forms...")
-  console.log(rows[0])
+  const responseSheetUrls = [
+    ...new Set(rows.map(row => row["Response spreadsheet URL"])),
+  ]
+
+  // 3. for each response spreadsheet, pull it in as text, convert it to json and loop through the rows, adding a new workflow for each
+  responseSheetUrls.forEach(sheetUrl => {
+    // TODO: fetch sheet here somehow
+    // let sheet
+    // sheet.forEach(row => {
+    //   db.workflow.create({})
+    // })
+  })
+
+  console.log(`✅ Done`)
 }
 
 run()
