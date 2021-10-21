fa78a7e0daa670077859ba62c1ede959ab04a737
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-10-21 15:35:04 +0100

    Revert "historic data import updates"
    This reverts commit d017727196cf2638f31e47151656a0a846d5ba64.
    Done because the build pipeline is broken

--- jobs/import-historic-data.js
+++ jobs/import-historic-data.js
@@ -18,10 +18,10 @@ const getIdFromUrl = url => {
 const run = async () => {
   try {
     // 0. set up a database connection and authenticate with google sheets api
-    console.log("📡 1/5 Connecting to DB...")
+    console.log("Connecting to DB...")
     const db = new PrismaClient()
 
-    console.log("🔐 2/5 Authenticating with Google...")
+    console.log("Authenticating with Google...")
     const auth = new google.auth.JWT(
       token.client_email,
       null,
@@ -32,59 +32,36 @@ const run = async () => {
     google.options({ auth })
 
     // 1. fetch mapping sheet
-    console.log("🗺 3/5 Fetching mapping data...")
+    console.log("Fetching mapping data...")
     const res = await fetch(process.env.HISTORIC_MAPPING_DATA_SOURCE)
     const text = await res.text()
     const allRows = await csv().fromString(text)
-    const mappings = allRows.slice(1)
+    const rows = allRows.slice(1)
 
     // 2. get a list of all the unique response spreadsheets
-    console.log("📑 4/5 Finding source form IDs...")
+    console.log("Finding source forms...")
     const responseSheetIds = [
       ...new Set(
-        mappings
-          .map(row => ({
-            url: row["Response spreadsheet URL"],
-            id: getIdFromUrl(row["Response spreadsheet URL"]),
-          }))
+        rows
+          .map(row => getIdFromUrl(row["Response spreadsheet URL"]))
           .filter(id => id)
       ),
     ]
 
     // 3. for each response spreadsheet, pull it in as text, convert it to json and loop through the rows, adding a new workflow for each
-    console.log("💾 5/5 Building and saving new workflows...")
-    await responseSheetIds.forEach(async responseSheet => {
+    await responseSheetIds.forEach(async spreadsheetId => {
       const responses = await sheets.spreadsheets.values.get({
-        spreadsheetId: responseSheet.id,
-        range: "A1:Z10000",
+        spreadsheetId,
+        range: "Sheet1!A1:Z1000",
       })
-
       responses.forEach(response => {
-        const answers = {}
-
-        // mappings
-        //   .filter(
-        //     mapping => mapping["Response spreadsheet URL"] === responseSheet.url
-        //   )
-        //   .forEach(mapping => {
-        //     answers[mapping["New step name"]][mapping["New field name"]] =
-        //       response[mapping["Old column name"]]
-        //   })
-
-        // await db.workflow.create({
-        //   answers,
-        //   socialCareId: ,
-        //   createdAt: ,
-        //   createdBy:,
-        //   submittedAt: ,
-        //   submittedBy:,
-        //   managerApprovedBy: ,
-        //   reviewBefore: ,
-        // })
+        console.log(response)
+        // TODO: apply mapping logic to the new workflow here
+        db.workflow.create({})
       })
     })
 
-    console.log(`\n ✅ Done`)
+    console.log(`✅ Done`)
   } catch (e) {
     console.error(e)
   }
