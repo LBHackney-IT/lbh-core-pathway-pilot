627f74922ddb66eb4acdb20b0adca66e8773bbcd
Wen Ting Wang <wenting@madetech.com>
2021-11-01 11:13:33 +0000

    Update reporting exporter to stringify arrays and objects

--- apps/reporting-exporter.js
+++ apps/reporting-exporter.js
@@ -1,18 +1,18 @@
-const {PrismaClient} = require("@prisma/client");
-const {Transform, Writable} = require("stream");
-const {google} = require('googleapis');
-const get = require('lodash.get');
+const { PrismaClient } = require("@prisma/client")
+const { Transform, Writable } = require("stream")
+const { google } = require("googleapis")
+const get = require("lodash.get")
 
-const BATCH_SIZE = 200;
+const BATCH_SIZE = 200
 
-const REPORTS = require('../config/reports.json');
+const REPORTS = require("../config/reports.json")
 
 class DataExtractor extends Transform {
-  report = [];
+  report = []
 
   constructor(report) {
-    super();
-    this.report = report;
+    super()
+    this.report = report
   }
 
   _transform(chunk, encoding, callback) {
@@ -20,53 +20,60 @@ class DataExtractor extends Transform {
       callback(
         null,
         JSON.stringify(
-          this.report.columns
-            .map(col => get(JSON.parse(chunk.toString()), col, '')),
-        ),
-      );
+          this.report.columns.map(col =>
+            get(JSON.parse(chunk.toString()), col, "")
+          )
+        )
+      )
     } catch (e) {
-      callback(e);
+      callback(e)
     }
   }
 }
 
 class StreamToSheet extends Writable {
-  items = [];
-  report;
-  spreadsheetId;
+  items = []
+  report
+  spreadsheetId
 
-  constructor({report, sheets}) {
-    super();
-    this.report = report;
-    this.sheets = sheets;
+  constructor({ report, sheets }) {
+    super()
+    this.report = report
+    this.sheets = sheets
   }
 
   _write(chunk, encoding, callback) {
-    this.items.push(chunk.toString());
+    this.items.push(chunk.toString())
 
     if (this.items.length >= BATCH_SIZE) {
-      this.flush().then(() => callback(null));
+      this.flush().then(() => callback(null))
     } else {
-      callback(null);
+      callback(null)
     }
   }
 
   async flush() {
-    const {spreadsheetId} = this.report;
+    const { spreadsheetId } = this.report
 
     const values = this.items
       .splice(0, BATCH_SIZE)
-      .map(r => !!r && JSON.parse(r))
-      .filter(r => !!r);
+      .map(r =>
+        JSON.parse(r).map(value =>
+          Array.isArray(value) || typeof value === "object"
+            ? JSON.stringify(value)
+            : value
+        )
+      )
+      .filter(r => !!r)
 
-    console.log(`[${this.report.name}]`, 'sending', values.length, 'rows');
+    console.log(`[${this.report.name}]`, "sending", values.length, "rows")
 
     await this.sheets.spreadsheets.values.append({
       spreadsheetId,
       range: `${this.report.name}!A1:Z`,
       valueInputOption: "USER_ENTERED",
-      resource: {values}
-    });
+      resource: { values },
+    })
   }
 }
 
@@ -75,109 +82,120 @@ const authenticate = async () => {
     email: process.env.REPORTING_GOOGLE_EMAIL,
     key: process.env.REPORTING_GOOGLE_PRIVATE_KEY,
     scopes: [
-      'https://www.googleapis.com/auth/drive',
-      'https://www.googleapis.com/auth/drive.file',
-      'https://www.googleapis.com/auth/spreadsheets',
+      "https://www.googleapis.com/auth/drive",
+      "https://www.googleapis.com/auth/drive.file",
+      "https://www.googleapis.com/auth/spreadsheets",
     ],
-  });
+  })
 
-  await auth.authorize();
+  await auth.authorize()
 
-  google.options({auth});
-};
+  google.options({ auth })
+}
 
-const withLogging = (stream) => {
+const withLogging = stream => {
   if (process.env.DEBUG)
     stream
-      .on('data', () => console.log(`${stream.constructor.name}#data`))
-      .on('end', () => console.log(`${stream.constructor.name}#end`))
-      .on('close', () => console.log(`${stream.constructor.name}#close`));
+      .on("data", () => console.log(`${stream.constructor.name}#data`))
+      .on("end", () => console.log(`${stream.constructor.name}#end`))
+      .on("close", () => console.log(`${stream.constructor.name}#close`))
 
-  return stream;
-};
+  return stream
+}
 
 const runQuery = async (report, position, connection) => {
   return await connection[report.entity].findMany({
     ...report.query,
     take: BATCH_SIZE,
     skip: position,
-  });
-};
+  })
+}
 
 const queryUntilEmpty = async (report, connection, dataExtractor) => {
-  let position = 0;
-  let more = true;
+  let position = 0
+  let more = true
 
   while (more) {
     const items = await runQuery(report, position, connection)
 
     if (items.length > 0) {
-      console.log(`[${report.name}]`, 'adding', items.length, 'items');
-      for (const item of items) dataExtractor.write(JSON.stringify(item));
-      position += BATCH_SIZE;
+      console.log(`[${report.name}]`, "adding", items.length, "items")
+      for (const item of items) dataExtractor.write(JSON.stringify(item))
+      position += BATCH_SIZE
     } else {
-      dataExtractor.end();
-      more = false;
+      dataExtractor.end()
+      more = false
     }
   }
 }
 
-const executeReport = (report, connection, sheets) => new Promise((resolve, reject) => {
-  console.log(`[${report.name}]`, 'starting');
+const executeReport = (report, connection, sheets) =>
+  new Promise((resolve, reject) => {
+    console.log(`[${report.name}]`, "starting")
 
-  const dataExtractor = withLogging(new DataExtractor(report));
-  const sheetUploader = withLogging(new StreamToSheet({report, sheets}));
+    const dataExtractor = withLogging(new DataExtractor(report))
+    const sheetUploader = withLogging(new StreamToSheet({ report, sheets }))
 
-  dataExtractor.pipe(sheetUploader);
+    dataExtractor.pipe(sheetUploader)
 
-  dataExtractor.on('close', () => {
-    sheetUploader.flush()
+    dataExtractor.on("close", () => {
+      sheetUploader.flush().then(() => {
+        sheetUploader.destroy()
+        dataExtractor.destroy()
+        resolve()
+      })
+    })
+
+    sheets.spreadsheets
+      .batchUpdate({
+        spreadsheetId: report.spreadsheetId,
+        resource: {
+          requests: [
+            {
+              updateCells: {
+                range: { sheetId: report.sheetId },
+                fields: "userEnteredValue",
+              },
+            },
+            {
+              updateSheetProperties: {
+                fields: "title",
+                properties: {
+                  sheetId: report.sheetId,
+                  title: report.name,
+                },
+              },
+            },
+          ],
+        },
+      })
       .then(() => {
-        sheetUploader.destroy();
-        dataExtractor.destroy();
-        resolve();
-      });
-  });
-
-  sheets.spreadsheets.batchUpdate({
-    spreadsheetId: report.spreadsheetId,
-    resource: {
-      requests: [{
-        updateCells: {
-          range: {sheetId: report.sheetId},
-          fields: "userEnteredValue",
-        },
-      }, {
-        updateSheetProperties: {
-          fields: "title",
-          properties: {
-            sheetId: report.sheetId,
-            title: report.name,
-          },
-        },
-      }]
-    }
-  }).then(() => {
-    sheets.spreadsheets.values.append({
-      spreadsheetId: report.spreadsheetId,
-      range: `${report.name}!A1:Z`,
-      valueInputOption: "USER_ENTERED",
-      resource: {
-        values: [report.columns]
-      }
-    }).then(() => {
-      queryUntilEmpty(report, connection, dataExtractor).catch(reject);
-    });
-  });
-});
+        sheets.spreadsheets.values
+          .append({
+            spreadsheetId: report.spreadsheetId,
+            range: `${report.name}!A1:Z`,
+            valueInputOption: "USER_ENTERED",
+            resource: {
+              values: [report.columns],
+            },
+          })
+          .then(() => {
+            queryUntilEmpty(report, connection, dataExtractor).catch(reject)
+          })
+      })
+  })
 
 module.exports.handler = async () => {
-  const connection = new PrismaClient();
-  const sheets = google.sheets('v4');
+  const connection = new PrismaClient()
+  const sheets = google.sheets("v4")
 
-  await authenticate();
+  await authenticate()
 
-  await Promise.all(REPORTS[process.env.ENVIRONMENT].map(report => executeReport(report, connection, sheets)));
+  await Promise.all(
+    REPORTS[process.env.ENVIRONMENT].map(report =>
+      executeReport(report, connection, sheets)
+    )
+  )
 }
 
-// if (!module.parent) module.exports.handler().catch(console.error);
+// if (!module.parent) module.exports.handler().catch(console.error)
--- apps/reporting-exporter.test.js
+++ apps/reporting-exporter.test.js
@@ -1,45 +1,64 @@
-const {handler: reportingHandler} = require('./reporting-exporter');
-const {google} = require('googleapis');
-const {PrismaClient} = require('@prisma/client');
+const { handler: reportingHandler } = require("./reporting-exporter")
+const { google } = require("googleapis")
+const { PrismaClient } = require("@prisma/client")
 
-jest.mock('@prisma/client');
-jest.mock('googleapis');
-jest.mock('../config/reports.json', () => ({
-  test: [{
-    "name": "test-data",
-    "spreadsheetId": "12345abcde",
-    "entity": "test",
-    "sheetId": "123456",
-    "query": {},
-    "columns": [
-      "id",
-      "string",
-      "json.string",
-    ],
-  }],
-}));
+jest.mock("@prisma/client")
+jest.mock("googleapis")
+jest.mock("../config/reports.json", () => ({
+  test: [
+    {
+      name: "test-data",
+      spreadsheetId: "12345abcde",
+      entity: "test",
+      sheetId: "123456",
+      query: {},
+      columns: [
+        "id",
+        "string",
+        "json.string",
+        "arrayOfStrings",
+        "arrayOfObjects",
+        "deeplyNestedObjects",
+      ],
+    },
+  ],
+}))
 
-process.env.REPORTING_GOOGLE_EMAIL = 'test@services.account';
-process.env.REPORTING_GOOGLE_PRIVATE_KEY = 'test-secret-key';
-process.env.ENVIRONMENT = 'test';
-process.env.DEBUG = 'true';
+process.env.REPORTING_GOOGLE_EMAIL = "test@services.account"
+process.env.REPORTING_GOOGLE_PRIVATE_KEY = "test-secret-key"
+process.env.ENVIRONMENT = "test"
+process.env.DEBUG = "true"
 
-const mockAuthorize = jest.fn().mockResolvedValue({});
+const mockAuthorize = jest.fn().mockResolvedValue({})
 google.auth.JWT.mockImplementation(() => ({
   authorize: mockAuthorize,
-}));
+}))
 
-const mockFindMany = jest.fn()
-  .mockResolvedValueOnce([{id: 'a', string: 'string', json: {string: 'string'}}])
-  .mockResolvedValue([]);
+const mockFindMany = jest
+  .fn()
+  .mockResolvedValueOnce([
+    {
+      id: "a",
+      string: "string",
+      json: { string: "string" },
+      arrayOfStrings: ["string1", "string2"],
+      arrayOfObjects: [{ prop1: "value1", prop2: "value2" }],
+      deeplyNestedObjects: {
+        prop: {
+          nestedProp: { anotherNestedProp: { lastNestedProp: "value" } },
+        },
+      },
+    },
+  ])
+  .mockResolvedValue([])
 PrismaClient.mockImplementation(() => ({
   test: {
     findMany: mockFindMany,
   },
 }))
 
-const mockBatchUpdate = jest.fn().mockResolvedValue({});
-const mockValuesAppend = jest.fn().mockResolvedValue({});
+const mockBatchUpdate = jest.fn().mockResolvedValue({})
+const mockValuesAppend = jest.fn().mockResolvedValue({})
 google.sheets = jest.fn().mockImplementation(() => ({
   spreadsheets: {
     batchUpdate: mockBatchUpdate,
@@ -47,95 +66,122 @@ google.sheets = jest.fn().mockImplementation(() => ({
       append: mockValuesAppend,
     },
   },
-}));
+}))
 
-const consoleLog = console.log;
-const consoleError = console.error;
+const consoleLog = console.log
+const consoleError = console.error
 
 beforeAll(() => {
-  console.log = jest.fn();
-  console.error = jest.fn();
+  console.log = jest.fn()
+  console.error = jest.fn()
 })
 afterAll(() => {
-  console.log = consoleLog;
-  console.error = consoleError;
+  console.log = consoleLog
+  console.error = consoleError
 })
 
-describe('when extracting data from a single table', () => {
-  describe('normal operation', () => {
-    beforeAll(async () => await reportingHandler());
+describe("when extracting data from a single table", () => {
+  describe("normal operation", () => {
+    beforeAll(async () => await reportingHandler())
 
-    test('creates an appropriate JWT instance', () => {
+    test("creates an appropriate JWT instance", () => {
       expect(google.auth.JWT).toHaveBeenCalledWith({
-        email: 'test@services.account',
-        key: 'test-secret-key',
+        email: "test@services.account",
+        key: "test-secret-key",
         scopes: [
-          'https://www.googleapis.com/auth/drive',
-          'https://www.googleapis.com/auth/drive.file',
-          'https://www.googleapis.com/auth/spreadsheets',
+          "https://www.googleapis.com/auth/drive",
+          "https://www.googleapis.com/auth/drive.file",
+          "https://www.googleapis.com/auth/spreadsheets",
         ],
-      });
-    });
+      })
+    })
 
-    test('makes a call to google auth', () => {
-      expect(mockAuthorize).toHaveBeenCalled();
-    });
+    test("makes a call to google auth", () => {
+      expect(mockAuthorize).toHaveBeenCalled()
+    })
 
-    test('logs that the report has started', () => {
-      expect(console.log).toHaveBeenCalledWith('[test-data]', 'starting');
-    });
+    test("logs that the report has started", () => {
+      expect(console.log).toHaveBeenCalledWith("[test-data]", "starting")
+    })
 
-    test('runs queries until there are no more records', () => {
-      expect(mockFindMany).toHaveBeenCalledTimes(2);
-    });
+    test("runs queries until there are no more records", () => {
+      expect(mockFindMany).toHaveBeenCalledTimes(2)
+    })
 
-    test('deletes all records from the google sheet', () => {
+    test("deletes all records from the google sheet", () => {
       expect(mockBatchUpdate).toHaveBeenCalledWith({
-        "spreadsheetId": "12345abcde",
-        "resource": {
-          "requests": expect.arrayContaining([{
-            "updateCells": {
-              "fields": "userEnteredValue",
-              "range": {"sheetId": "123456"}
-            }
-          }])
+        spreadsheetId: "12345abcde",
+        resource: {
+          requests: expect.arrayContaining([
+            {
+              updateCells: {
+                fields: "userEnteredValue",
+                range: { sheetId: "123456" },
+              },
+            },
+          ]),
         },
-      });
-    });
+      })
+    })
 
-    test('ensures that the sheet has the right title', () => {
+    test("ensures that the sheet has the right title", () => {
       expect(mockBatchUpdate).toHaveBeenCalledWith({
-        "spreadsheetId": "12345abcde",
-        "resource": {
-          "requests": expect.arrayContaining([{
-            "updateSheetProperties": {
-              "fields": "title",
-              "properties": {
-                "sheetId": "123456",
-                "title": "test-data"
-              }
-            }
-          }])
+        spreadsheetId: "12345abcde",
+        resource: {
+          requests: expect.arrayContaining([
+            {
+              updateSheetProperties: {
+                fields: "title",
+                properties: {
+                  sheetId: "123456",
+                  title: "test-data",
+                },
+              },
+            },
+          ]),
         },
-      });
-    });
+      })
+    })
 
-    test('adds headers to the correct sheet', () => {
+    test("adds headers to the correct sheet", () => {
       expect(mockValuesAppend).toHaveBeenCalledWith({
-        "range": "test-data!A1:Z",
-        "resource": {"values": [["id", "string", "json.string"]]},
-        "spreadsheetId": "12345abcde",
-        "valueInputOption": "USER_ENTERED"
-      });
-    });
+        range: "test-data!A1:Z",
+        resource: {
+          values: [
+            [
+              "id",
+              "string",
+              "json.string",
+              "arrayOfStrings",
+              "arrayOfObjects",
+              "deeplyNestedObjects",
+            ],
+          ],
+        },
+        spreadsheetId: "12345abcde",
+        valueInputOption: "USER_ENTERED",
+      })
+    })
 
-    test('appends data to the correct sheet', () => {
+    test("appends data to the correct sheet", () => {
       expect(mockValuesAppend).toHaveBeenCalledWith({
-        "range": "test-data!A1:Z",
-        "resource": {"values": [["a", "string", "string"]]},
-        "spreadsheetId": "12345abcde",
-        "valueInputOption": "USER_ENTERED"
-      });
-    });
-  });
-});
+        range: "test-data!A1:Z",
+        resource: {
+          values: [
+            [
+              "a",
+              "string",
+              "string",
+              '["string1","string2"]',
+              '[{"prop1":"value1","prop2":"value2"}]',
+              '{"prop":{"nestedProp":{"anotherNestedProp":{"lastNestedProp":"value"}}}}',
+            ],
+          ],
+        },
+
+        spreadsheetId: "12345abcde",
+        valueInputOption: "USER_ENTERED",
+      })
+    })
+  })
+})
