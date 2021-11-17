aac591ca441a58baa5ef312d46ded9cd54326793
Wen Ting Wang <wenting@madetech.com>
2021-11-17 09:33:24 +0000

    Update tests and config to allow for usage of .env.*.local
    Also, update README and add new scripts to support this.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,10 +1,12 @@
-require('dotenv').config()
+require('dotenv-flow').config();
 
 /**
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
+  config.baseUrl = process.env.NEXTAUTH_URL
+
   config.env.HACKNEY_JWT_SECRET = process.env.HACKNEY_JWT_SECRET
   config.env.GSSO_TOKEN_NAME = process.env.GSSO_TOKEN_NAME
 
