d2edff160078abe203008579f567c4ecf94d558b
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-11-17 09:39:50 +0000

    Decorate the sentry output with user email address

--- lib/protectRoute.ts
+++ lib/protectRoute.ts
@@ -1,16 +1,17 @@
 import {GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult} from "next";
 import {unprotectedPages} from "../config";
 import {getSession} from "next-auth/client";
+import {setUser} from "@sentry/nextjs";
 
 export const protectRoute =
   (getServerSideProps: GetServerSideProps) =>
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     async (context: GetServerSidePropsContext): Promise<GetServerSidePropsResult<{ [key:string]: any }>> => {
       const {resolvedUrl, req} = context;
+      const session = await getSession({ req });
 
       if (
-        !unprotectedPages.includes(resolvedUrl)
-        && !await getSession({ req })
+        !unprotectedPages.includes(resolvedUrl) && !session
       ) return {
         props: {},
         redirect: {
@@ -19,6 +20,7 @@ export const protectRoute =
         },
       };
 
+      if (session) setUser({email: session.user.email});
 
       return getServerSideProps(context);
     };
