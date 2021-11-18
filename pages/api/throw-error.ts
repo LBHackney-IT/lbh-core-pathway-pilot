f99a8fdaaaf6c27e3599f9fc29e1e3e685fc4e52
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-11-18 14:54:42 +0000

    Ignore arguments not used in throw error api

--- pages/api/throw-error.ts
+++ pages/api/throw-error.ts
@@ -2,6 +2,7 @@ import {apiHandler, ApiRequestWithSession} from "../../lib/apiHelpers";
 import {NextApiResponse} from "next";
 import {middleware as csrfMiddleware} from "../../lib/csrfToken";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const handler = async (req: ApiRequestWithSession, res: NextApiResponse) => {
   throw Error("ErrorThrowingPage api error")
 };
