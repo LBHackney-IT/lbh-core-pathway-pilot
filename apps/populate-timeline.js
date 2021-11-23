254797573e10494fc690de102a5800ea1d3f7b9a
Wen Ting Wang <wenting@madetech.com>
2021-11-23 16:17:38 +0000

    Update getResidentById in populate-timeline to only return required values

--- apps/populate-timeline.js
+++ apps/populate-timeline.js
@@ -27,36 +27,8 @@ const getResidentById = async id => {
         mosaicId: String(residentFromSCCV.id),
         firstName: residentFromSCCV.firstName,
         lastName: residentFromSCCV.lastName,
-        gender: residentFromSCCV.gender,
         dateOfBirth: residentFromSCCV.dateOfBirth || null,
-        nhsNumber: residentFromSCCV.nhsNumber
-          ? String(residentFromSCCV.nhsNumber)
-          : null,
         ageContext: residentFromSCCV.contextFlag,
-        restricted: residentFromSCCV.restricted,
-        addressList: residentFromSCCV.address
-          ? [
-              {
-                addressLine1: residentFromSCCV.address?.address,
-                postCode: residentFromSCCV.address?.postcode,
-              },
-            ]
-          : [],
-        phoneNumber: residentFromSCCV.phoneNumbers.map(phoneNumber => ({
-          phoneNumber: phoneNumber.number,
-          phoneType: phoneNumber.type,
-        })),
-        ethnicity: residentFromSCCV.ethnicity || null,
-        firstLanguage: residentFromSCCV.firstLanguage || null,
-        religion: residentFromSCCV.religion || null,
-        sexualOrientation: residentFromSCCV.sexualOrientation || null,
-        emailAddress: residentFromSCCV.emailAddress || null,
-        preferredMethodOfContact:
-          residentFromSCCV.preferredMethodOfContact || null,
-        otherNames: residentFromSCCV.otherNames.map(otherName => ({
-          firstName: otherName.firstName,
-          lastName: otherName.lastName,
-        })),
       }
     }
   } catch (e) {
