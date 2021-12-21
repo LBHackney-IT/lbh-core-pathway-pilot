77be9a3c12910cfa90d86c9a07f5a953d2c78a01
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2021-12-21 14:17:37 +0000

    Add checks for authenticated access

--- cypress/integration/authentication.js
+++ cypress/integration/authentication.js
@@ -1,14 +1,13 @@
 describe("visiting the app logged in", () => {
   it("can visit the website without being redirected", () => {
     cy.visitAsUser("/");
+    cy.location('hostname').should('eq', 'localhost')
   })
 })
 
 describe("visiting the app when not logged in", () => {
   it("redirected to the auth server", () => {
-    cy.visit("/", {
-      failOnStatusCode: false,
-    });
-    cy.location('host').should('eq', 'example.com')
+    cy.visit("/", {failOnStatusCode: false});
+    cy.location('hostname').should('eq', 'example.com')
   })
 })
