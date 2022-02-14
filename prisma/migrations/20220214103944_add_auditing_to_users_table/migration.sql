e54222f06e2f026d23a2e3573c256117f5d92c61
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-02-14 12:10:12 +0000

    Include the record id in the audit table

--- prisma/migrations/20220214103944_add_auditing_to_users_table/migration.sql
+++ prisma/migrations/20220214103944_add_auditing_to_users_table/migration.sql
@@ -3,6 +3,7 @@ CREATE TABLE "User_Audit"
 (
     operation char(20) NOT NULL,
     timestamp timestamp DEFAULT CURRENT_TIMESTAMP,
+    id        text     NOT NULL,
     before    JSONB    NOT NULL,
     after     JSONB    NOT NULL
 );
@@ -11,14 +12,14 @@ CREATE OR REPLACE FUNCTION User_Audit_Trigger() RETURNS TRIGGER AS
 $User_Audit$
 BEGIN
     IF (TG_OP = 'DELETE') THEN
-        INSERT INTO "User_Audit" (operation, before, after)
-        VALUES ('DELETE', TO_JSONB(old), '"null"');
+        INSERT INTO "User_Audit" (operation, id, before, after)
+        VALUES ('DELETE', old.id, TO_JSONB(old), '"null"');
     ELSIF (TG_OP = 'INSERT') THEN
-        INSERT INTO "User_Audit" (operation, before, after)
-        VALUES ('INSERT', '"null"', TO_JSONB(new));
+        INSERT INTO "User_Audit" (operation, id, before, after)
+        VALUES ('INSERT', new.id, '"null"', TO_JSONB(new));
     ELSIF (TG_OP = 'UPDATE') THEN
-        INSERT INTO "User_Audit" (operation, before, after)
-        VALUES ('UPDATE', TO_JSONB(old), TO_JSONB(new));
+        INSERT INTO "User_Audit" (operation, id, before, after)
+        VALUES ('UPDATE', old.id, TO_JSONB(old), TO_JSONB(new));
     END IF;
     RETURN NULL;
 END;
