29be35b62db63f39aea31295baf40b535c007607
Rachel Hutchinson <rachelhutchinson93@gmail.com>
2022-03-15 09:32:10 +0000

    Fix inital test

--- components/TaskList.test.tsx
+++ components/TaskList.test.tsx
@@ -1,11 +1,10 @@
 import { render } from "@testing-library/react"
 import { mockWorkflowWithExtras } from "../fixtures/workflows"
-import { mockForm } from "../fixtures/form"
 import TaskList from "./TaskList"
 
 describe("Task List Component Tests", () => {
   it("should render the Task List component properly", () => {
-    const taskList = render(<TaskList workflow={mockWorkflowWithExtras} />);
-    // expect(taskList).not.toBe(null);
+    const { getAllByText } = render(<TaskList workflow={mockWorkflowWithExtras} />);
+    expect(getAllByText('1. Mock theme')).not.toBe(null);
   })
 })
