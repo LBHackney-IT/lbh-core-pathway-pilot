3c30370f697cb4000ae389f2ce2cd4bc52301d70
Alexandra Herbert <Alexandra.Herbert@Madetech.com>
2022-03-16 14:28:31 +0000

    WIP - Refactoring our changes out of the regular pipeline to see if the tests pass again - they do.  Made a mew method which should be congruent with the status setting function we modified earlier.  Need to see if we can use this new async status code in the one place we need it to save on having to change a bunch of code and tests around.

--- lib/status.ts
+++ lib/status.ts
@@ -6,7 +6,7 @@ import forms from "../config/forms"
 import { prettyDateToNow } from "./formatters"
 
 /** determine the current stage of a workflow for logic */
-export const getStatus = async (workflow: Workflow): Promise<Status> => {
+export const getStatus = (workflow: Workflow): Status => {
   // the order of these determines priority
   if (workflow.discardedAt) return Status.Discarded
 
@@ -29,21 +29,27 @@ export const getStatus = async (workflow: Workflow): Promise<Status> => {
     }
   }
 
-  const formData = await forms()
-  const thisForm = formData.find(form => form.id === workflow.formId)
-  if (!thisForm.approvable) {
-    return Status.NoAction
-  }
-
   if (workflow.managerApprovedAt) {
     if (workflow.needsPanelApproval) {
       return Status.ManagerApproved
     } else {
+
       return Status.NoAction
     }
   }
   if (workflow.submittedAt) return Status.Submitted
   return Status.InProgress
+}
+
+export const getStatusBasedOnApprovability = async (workflow: Workflow): Promise<Status> => {
+  if (workflow.discardedAt) return Status.Discarded
+  const formData = await forms()
+  const thisForm = formData.find(form => form.id === workflow.formId)
+  if (!thisForm.approvable) {
+    return Status.NoAction
+  } else {
+    return getStatus(workflow)
+  }
 }
 
 /** get status of a workflow for display */
