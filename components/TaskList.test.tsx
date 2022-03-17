6599a4f1f6d233d7eee632efb2de340787bc5529
Rachel Hutchinson <rachelhutchinson93@gmail.com>
2022-03-17 11:14:56 +0000

    Remove feature flag

--- components/TaskList.test.tsx
+++ components/TaskList.test.tsx
@@ -3,15 +3,6 @@ import { mockForm } from "../fixtures/form"
 import { mockWorkflowWithExtras } from "../fixtures/workflows"
 import TaskList from "./TaskList"
 
-const switchEnv = environment => {
-  const oldEnv = process.env.NODE_ENV
-  process.env.NEXT_PUBLIC_ENV = environment
-  // @ts-ignore
-  process.env.NODE_ENV = environment
-
-  return () => switchEnv(oldEnv)
-}
-
 const extraFieldsForm = {
   ...mockForm,
   themes: [
@@ -72,61 +63,19 @@ const mockWorkflowWithUpdatedForm = {
 }
 
 describe("Task List Component Tests in development", () => {
-  let switchBack
-
-  beforeAll(() => (switchBack = switchEnv("development")))
   beforeEach(async () => {
     await act(async () => {
       await render(<TaskList workflow={mockWorkflowWithUpdatedForm} />)
     })
   })
-  afterAll(() => switchBack())
 
-  it("should show a theme if typeFilter matches the workflow type", () => {
+  it("should show a theme if there is only one typeFilter and this matches the workflow type", () => {
     expect(screen.getAllByText("3. Mock theme 4")).not.toBe(null)
   })
   it("should not show a theme if there is no typeFilter", () => {
     expect(screen.queryByText("3. Mock theme 3")).toBe(null)
   })
-  it("should show a theme if there are multiple typeFilter and one of them matches the workflow type", () => {
+  it("should show a theme if there are multiple typeFilters and one of them matches the workflow type", () => {
     expect(screen.queryByText("2. Mock theme 2")).not.toBe(null)
-  })
-})
-
-describe("Task List Component Tests in test", () => {
-  let switchBack
-
-  beforeAll(() => (switchBack = switchEnv("test")))
-  beforeEach(async () => {
-    await act(async () => {
-      await render(<TaskList workflow={mockWorkflowWithUpdatedForm} />)
-    })
-  })
-  afterAll(() => switchBack())
-
-  it("should show a theme if the typeFilter matches the workflow type", () => {
-    expect(screen.getAllByText("3. Mock theme 4")).not.toBe(null)
-  })
-  it("should not show a theme if there is no typeFilter", () => {
-    expect(screen.queryByText("3. Mock theme 3")).toBe(null)
-  })
-})
-
-describe("Task List Component Tests in production", () => {
-  let switchBack
-
-  beforeAll(() => (switchBack = switchEnv("production")))
-  beforeEach(async () => {
-    await act(async () => {
-      await render(<TaskList workflow={mockWorkflowWithUpdatedForm} />)
-    })
-  })
-  afterAll(() => switchBack())
-
-  it("should show a theme if the typeFilter matches the workflow type", () => {
-    expect(screen.getAllByText("4. Mock theme 4")).not.toBe(null)
-  })
-  it("should show a theme if there is no typeFilter", () => {
-    expect(screen.queryByText("3. Mock theme 3")).not.toBe(null)
   })
 })
--- components/TaskList.tsx
+++ components/TaskList.tsx
@@ -21,13 +21,9 @@ export const retrieveFilterThemes = (
 ): Theme[] => {
   let filteredThemes
   if (workflow?.form?.themes) {
-    if (process.env.NODE_ENV === "production") {
-      filteredThemes = workflow.form.themes
-    } else {
-      filteredThemes = workflow.form.themes.filter(theme =>
-        theme.typeFilter ? theme.typeFilter.includes(workflow.type) : false
-      )
-    }
+    filteredThemes = workflow.form.themes.filter(theme =>
+      theme.typeFilter ? theme.typeFilter.includes(workflow.type) : false
+    )
     return filteredThemes
   } else {
     return []
