f21d0ad40f5456e8030cfe9f29a9e3cb9e283831
Rachel Hutchinson <rachelhutchinson93@gmail.com>
2022-03-18 00:15:29 +0000

    Show reviews on the blue tags

--- components/WorkflowPanel.tsx
+++ components/WorkflowPanel.tsx
@@ -38,6 +38,7 @@ const WorkflowPanel = ({ workflow }: Props): React.ReactElement => {
     reassessment = workflow.nextWorkflows.find(
     w => w.type === WorkflowType.Reassessment
   ))
+  console.log('workflow.type', workflow.type)
 
   return (
     <div className={workflow.heldAt ? `${s.outer} ${s.held}` : s.outer}>
@@ -48,9 +49,14 @@ const WorkflowPanel = ({ workflow }: Props): React.ReactElement => {
           ) : (
             <span className={s.placeholder}>{workflow.socialCareId}</span>
           )}
-          {(workflow.type === "Reassessment" || workflow.type === "Review") && (
+          {(workflow.type === "Reassessment") && (
             <span className={`govuk-tag lbh-tag lbh-tag--blue  ${s.review}`}>
               Reassessment
+            </span>
+          )}
+          {(workflow.type === "Review") && (
+            <span className={`govuk-tag lbh-tag lbh-tag--blue  ${s.review}`}>
+              Review
             </span>
           )}
           {workflow.type === "Historic" && (
