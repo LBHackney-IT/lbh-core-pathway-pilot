92fcab7573c76de21da5f057195a68a2e3936cf8
Alexandra Herbert <Alexandra.Herbert@Madetech.com>
2022-04-01 12:28:58 +0100

    Adding a new method to ascertain initals from email address
    #Wired this into KanbanCard where the inital issue was breaking the app
    
    Need to write tests for kanban card when the user name is null or empty string
    
    Need to look for anywhere else that userInitals is called and give it the same treatment
    
    ~Phoenix Ryder

--- lib/formatters.test.ts
+++ lib/formatters.test.ts
@@ -3,14 +3,14 @@ import localNextStepOptions from "../config/nextSteps/nextStepOptions.json"
 
 import {
   displayEditorNames,
-  displayEthnicity,
+  displayEthnicity, emailInitials,
   prettyDate,
   prettyDateAndTime,
   prettyDateToNow,
   prettyGmailMessage,
   prettyNextSteps,
   prettyResidentName,
-  truncate,
+  truncate, userInitials,
 } from "./formatters"
 
 jest
@@ -173,6 +173,14 @@ describe("displayEthnicity", () => {
   })
 })
 
+describe("userInitials", () => {
+  it("properly splits a name String into initials", () => {
+    const actual = userInitials("Olatokunbo Koiki ")
+    const expected = "OK"
+    expect(actual).toBe(expected)
+  })
+})
+
 describe("prettyGmailMessage", () => {
   it("correctly formats a message", () => {
     const result = prettyGmailMessage({
@@ -190,5 +198,20 @@ describe("prettyGmailMessage", () => {
   ---
   eyup
   ---`)
+  })
+})
+
+describe("emailInitials", () => {
+  it("ascertains initials from email address", () => {
+    expect(emailInitials("Phoenix.Ryder@hackney.gov.uk")). toBe("PR")
+  })
+
+  it("ascertains single initial from email address", () => {
+    expect(emailInitials("Phoenix@hackney.gov.uk")). toBe("P")
+  })
+
+  it("returns empty string when falsey email supplied", () => {
+    expect(emailInitials("")). toBe("??")
+    expect(emailInitials(null)). toBe("??")
   })
 })
