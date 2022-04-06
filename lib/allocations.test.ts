d12ca6cb76413f7a513947a529556057d7c9f5c6
Miles-Alford <miles.alford@hackney.gov.uk>
2022-04-06 14:04:22 +0100

    Allow api key to be undefined and fix type casting
    Co-authored-by: Lawrence <acodeninja@users.noreply.github.com>

--- lib/allocations.test.ts
+++ lib/allocations.test.ts
@@ -6,9 +6,15 @@ jest.mock('node-fetch', () => jest.fn().mockResolvedValue({
 }));
 describe('allocations', () => {
   let response;
+  const api_key = process.env.SOCIAL_CARE_API_KEY
   beforeAll(async () => {
+    process.env.SOCIAL_CARE_API_KEY = "test-api-key";
     response = await getAllocationsByEmail("test@example.com");
   });
+
+  afterAll(() => {
+    process.env.SOCIAL_CARE_API_KEY = api_key
+  })
   it("calls the social care api with the correct data", () => {
     expect(fetch).toHaveBeenCalledWith(
       "https://virtserver.swaggerhub.com/Hackney/social-care-case-viewer-api/1.0.0/allocations?worker_email=test@example.com&status=open",
