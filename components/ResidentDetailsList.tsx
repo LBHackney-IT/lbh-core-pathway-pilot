1b18164d56e7559db44bca4ee0a39272fa589935
Rachel Hutchinson <rachelhutchinson93@gmail.com>
2022-04-07 10:04:25 +0100

    Updating confirm person details page

--- components/ResidentDetailsList.tsx
+++ components/ResidentDetailsList.tsx
@@ -25,56 +25,140 @@ interface Props {
 
 const ResidentDetailsList = ({ socialCareId }: Props): React.ReactElement => {
   const { data: resident } = useSuperResident(socialCareId)
-  
-  // const {
-  //   // mosaicId,
-  //   firstName,
-  //   lastName,
-  //   dateOfBirth,
-  //   ageContext,
-  //   gender,
-  //   // phoneNumber,
-  //   // addressList,
-  //   nhsNumber,
-  //   otherNames,
-  //   firstLanguage,
-  //   emailAddress,
-  //   preferredMethodOfContact,
-  //   allocatedTeam,
-  //   fluentInEnglish,
-  // } = data
 
-  if (resident) {return (
-    <dl className="govuk-summary-list lbh-summary-list govuk-!-margin-top-6  govuk-!-margin-bottom-8">
-      <BasicRow label="Name" value={`${resident.firstName} ${resident.lastName}`} />
-      <BasicRow label="Name" value={`${resident.allocatedTeam} ${resident.lastName}`} />
-
-      {/* <div className="govuk-summary-list__row">
-        <dt className="govuk-summary-list__key">Other names</dt>
-        <dd className="govuk-summary-list__value">
-          {otherNames?.length > 0 ? (
-            <ul className="lbh-list">
-              {otherNames.map(({ firstName, lastName }) => (
-                <li key={`${firstName} ${lastName}`}>
-                  {firstName} {lastName}
+  if (resident) {
+    return (
+      <dl className="govuk-summary-list lbh-summary-list govuk-!-margin-top-6  govuk-!-margin-bottom-8">
+        <BasicRow label="Social care ID" value={String(resident.id)} />
+        <BasicRow
+          label="Service area"
+          value={resident.ageContext === "C" ? "Children" : "Adults"}
+        />
+        <BasicRow label="Allocated team" value={resident.allocatedTeam} />
+        <BasicRow label="Title" value={resident.title} />
+        <BasicRow label="First name" value={resident.firstName} />
+        <BasicRow label="Last name" value={resident.lastName} />
+        <div className="govuk-summary-list__row">
+          <dt className="govuk-summary-list__key">Other names</dt>
+          <dd className="govuk-summary-list__value">
+            {resident.otherNames?.length > 0 ? (
+              <ul className="lbh-list">
+                {resident.otherNames.map(({ firstName, lastName }) => (
+                  <li key={`${firstName} ${lastName}`}>
+                    {firstName} {lastName}
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <Unknown />
+            )}
+          </dd>
+        </div>
+        <BasicRow
+          label="Date of birth"
+          value={prettyDate(resident.dateOfBirth)}
+        />
+        <BasicRow label="Died" value={prettyDate(resident.dateOfDeath)} />
+        <BasicRow label="Gender" value={resident.gender} /> {/* //convert? */}
+        <BasicRow
+          label="Same gender as assigned at birth?"
+          value={resident.genderAssignedAtBirth ? "Yes" : "No"}
+        />
+        <BasicRow label="Pronoun" value={resident.pronoun} />
+        <BasicRow
+          label="Sexual orientation"
+          value={resident.sexualOrientation}
+        />
+        <BasicRow label="Ethnicity" value={resident.ethnicity} />{" "}
+        {/* //convert? */}
+        <BasicRow label="Email address" value={resident.emailAddress} />
+        <div className="govuk-summary-list__row">
+          <dt className="govuk-summary-list__key">Phone numbers</dt>
+          <dd className="govuk-summary-list__value">
+            {resident.phoneNumbers?.length > 0 ? (
+              <ul className="lbh-list">
+                {resident.phoneNumbers.map(({ type, number }) => (
+                  <li key={number}>
+                    <strong>{type}</strong>, {number}
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <Unknown />
+            )}
+          </dd>
+        </div>
+        <BasicRow
+          label="Restricted?"
+          value={resident.restricted === "Y" ? "Yes" : "No"}
+        />
+        <BasicRow label="Religion" value={resident.religion} />
+        <BasicRow label="Employment" value={resident.employment} />
+        <BasicRow label="Marital status" value={resident.maritalStatus} />
+        <BasicRow
+          label="Immigration status"
+          value={resident.immigrationStatus}
+        />
+        <BasicRow label="Care provider" value={resident.careProvider} />
+        <BasicRow
+          label="Primary support reason"
+          value={resident.primarySupportReason}
+        />
+        {/*  Health and disability*/}
+        <BasicRow
+          label="NHS number"
+          value={resident.nhsNumber ? String(resident.nhsNumber) : ""}
+        />
+        <div>
+          <ul className="lbh-list lbh-body-s ">
+            <li className="govuk-!-margin-top-0">
+              <strong>{resident.gpDetails?.name}</strong>
+            </li>
+            <li className="govuk-!-margin-top-0">
+              {resident.gpDetails?.address}
+            </li>
+            <li className="govuk-!-margin-top-0">
+              {resident.gpDetails?.postcode}
+            </li>
+            <li className="govuk-!-margin-top-0">
+              <a href={`mailto:${resident.gpDetails?.email}`}>
+                {resident.gpDetails?.email}
+              </a>
+            </li>
+            <li className="govuk-!-margin-top-0">
+              {resident.gpDetails?.phoneNumber}
+            </li>
+          </ul>
+        </div>
+        <div className="govuk-summary-list__row">
+          <dt className="govuk-summary-list__key">GP</dt>
+          <dd className="govuk-summary-list__value">
+            {resident.gpDetails ? (
+              <ul className="lbh-list lbh-body-s ">
+                <li className="govuk-!-margin-top-0">
+                  <strong>{resident.gpDetails?.name}</strong>
                 </li>
-              ))}
-            </ul>
-          ) : (
-            <Unknown />
-          )}
-        </dd>
-      </div>
-
-      <BasicRow label="Team" value={allocatedTeam} />
-      <BasicRow
-        label="Fluent in English"
-        value={fluentInEnglish ? "Yes" : "No"}
-      />
-      <BasicRow label="Social care ID" value={mosaicId} />
-      <BasicRow label="Gender" value={gender} />
-      <BasicRow label="Date of birth" value={prettyDate(dateOfBirth)} />
-      <BasicRow label="First language" value={firstLanguage} />
+                <li className="govuk-!-margin-top-0">
+                  {resident.gpDetails?.address}
+                </li>
+                <li className="govuk-!-margin-top-0">
+                  {resident.gpDetails?.postcode}
+                </li>
+                <li className="govuk-!-margin-top-0">
+                  <a href={`mailto:${resident.gpDetails?.email}`}>
+                    {resident.gpDetails?.email}
+                  </a>
+                </li>
+                <li className="govuk-!-margin-top-0">
+                  {resident.gpDetails?.phoneNumber}
+                </li>
+              </ul>
+            ) : (
+              <Unknown />
+            )}
+          </dd>
+        </div>
+        {/* <BasicRow label="GP" value={resident.gpDetails} />
 
       <div className="govuk-summary-list__row">
         <dt className="govuk-summary-list__key">Addresses</dt>
@@ -95,22 +179,7 @@ const ResidentDetailsList = ({ socialCareId }: Props): React.ReactElement => {
         </dd>
       </div>
 
-      <div className="govuk-summary-list__row">
-        <dt className="govuk-summary-list__key">Phone numbers</dt>
-        <dd className="govuk-summary-list__value">
-          {phoneNumber?.length > 0 ? (
-            <ul className="lbh-list">
-              {phoneNumber.map(({ phoneType, phoneNumber }) => (
-                <li key={phoneNumber}>
-                  <strong>{phoneType}</strong>, {phoneNumber}
-                </li>
-              ))}
-            </ul>
-          ) : (
-            <Unknown />
-          )}
-        </dd>
-      </div>
+      
 
       <BasicRow label="Email address" value={emailAddress} />
       <BasicRow
@@ -122,10 +191,9 @@ const ResidentDetailsList = ({ socialCareId }: Props): React.ReactElement => {
         value={ageContext === "C" ? "Children" : "Adults"}
       />
       <BasicRow label="NHS number" value={nhsNumber} /> */}
-
-
-    </dl>
-  )} 
+      </dl>
+    )
+  }
   return null
 }
 
