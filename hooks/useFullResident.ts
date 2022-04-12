4bd35148d1d8da25b018ec66d75719f9a1a8518a
Rachel Hutchinson <rachelhutchinson93@gmail.com>
2022-04-12 12:41:27 +0100

    Refactor code to use one api rather than two

--- hooks/useFullResident.ts
+++ hooks/useFullResident.ts
@@ -1,10 +1,7 @@
 import useSWR, { SWRResponse } from "swr"
 import { FullResident } from "../components/ResidentDetailsList.types"
 
-//Change to fullResident
-//Change to one api that takes in a parameter
-//Answer filters - look at this - might be useful for conditionally showing - Frontend as reference material
 const useFullResident = (socialCareId: string): SWRResponse<FullResident, Error> =>
-  useSWR(`/api/superresidents/${socialCareId}`)
+  useSWR(`/api/residents/${socialCareId}?fullView=true`)
 
 export default useFullResident
--- pages/api/residents/[id].ts
+++ pages/api/residents/[id].ts
@@ -1,11 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { apiHandler } from "../../../lib/apiHelpers"
-import { getResidentById } from "../../../lib/residents"
+import { getFullResidentById, getResidentById } from "../../../lib/residents"
 import { middleware as csrfMiddleware } from '../../../lib/csrfToken';
 
+//add parameter - view = full - then use getFullResidentById
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { id } = req.query
-  const resident = await getResidentById(id as string)
+  const { id, fullView } = req.query
+  const resident =  fullView ? await getFullResidentById(id as string) : await getResidentById(id as string)
   res.json(resident)
 }
 
